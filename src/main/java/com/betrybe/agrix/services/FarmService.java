347f74e143f5306b78d03b85aa5ee2df4a2f857f
Matheus Milanez <mattmila96@gmail.com>
2023-10-17 17:57:33 -0300

    requisito 1 teste

---
+++ src/main/java/com/betrybe/agrix/services/FarmService.java
@@ -0,0 +1,24 @@
+package com.betrybe.agrix.services;
+
+import com.betrybe.agrix.models.entities.Farm;
+import com.betrybe.agrix.models.repositories.FarmRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * FarmService.
+ */
+@Service
+public class FarmService {
+
+  private final FarmRepository farmRepository;
+
+  @Autowired
+  public FarmService(FarmRepository farmRepository) {
+    this.farmRepository = farmRepository;
+  }
+
+  public Farm insertFarm(Farm farm) {
+    return farmRepository.save(farm);
+  }
+}
